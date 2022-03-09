eb95cef0d609f9dffffe34658abd9a7abbba1786
DrPeterPun <pedro_pereira@outlook.pt>
2022-03-09 19:27:36 +0000

    aula 3

--- TP3/mix_cursos_pubs.js
+++ TP3/mix_cursos_pubs.js
@@ -4,14 +4,14 @@ axios.get('http://localhost:3001/cursos')
     .then(resp => {
         cursos = resp.data;
         cursos.forEach(a => {
-        axios.post('http://localhost:3000/pubs', {
-            "id" : a.id,
-            "title": a.designacao
-        }).then(resp =>
-            console.log(resp.data))
-        .catch(error => {
-            console.log('Error: ' + error);
-        });
+            axios.post('http://localhost:3000/pubs', {
+                "id" : a.id,
+                "title": a.designacao
+            }).then(resp =>
+                console.log("registo inserido :" + resp.data))
+            .catch(error => {
+                console.log('Error: ' + error);
+            });
         });
     })
     .catch(error => {
